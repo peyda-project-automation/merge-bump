61defc1913ab801273e46dab1c291cf1bc84fbe9
Ian MacLeod <ian@nevir.net>
2020-09-12 16:55:45 -0700

    Print out event details

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+const fs = require('fs');
+
 main();
 async function main() {
-  console.log('test');
+  process.stdout.write(`env:\n`);
   process.stdout.write(JSON.stringify(process.env, null, 2) + '\n');
+
+  process.stdout.write(`event:\n`);
+  const event = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_FILE, 'utf8'));
+  process.stdout.write(JSON.stringify(event, null, 2) + '\n');
 }
